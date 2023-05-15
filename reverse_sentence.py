4db0a676fc6a55f4ae1cc8c7002f86e71cc0df50
yesiratnasari18 <133626799+yesiratnasari18@users.noreply.github.com>
2023-05-15 12:32:54 +0700

    Add files via upload

---
+++ reverse_sentence.py
@@ -0,0 +1,14 @@
+def reverse_sentence(sentence):
+    stack = []
+    reversed_sentence=""
+
+    for word in sentence.split():
+        stack.append(word)
+
+    while len(stack)>0:
+        reversed_sentence += stack.pop()+""
+
+    return reversed_sentence.strip()
+
+sentence = "Selamat pagi, bagaimana kabar Anda?"
+print(reverse_sentence(sentence))
